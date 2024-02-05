72a15fdfa39307eb24e16c7453c9dbc6b7af26b6
Maxwell Dalboni <maxwell@dalboni.land>
2024-02-04 22:06:49 -0300

    removing unused code
    removed old experimental code.

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -3,58 +3,14 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/IBM/sarama"
 )
 
 var (
-	brokerList        = []string{"localhost:9093"}
-	topic             = "important"
-	partition         = "0"
-	offsetType        = -1
-	messageCountStart = 0
+	brokerList = []string{"localhost:9093"}
+	topic      = "important"
 )
-
-func main2() {
-	config := sarama.NewConfig()
-	config.Consumer.Return.Errors = true
-	brokers := brokerList
-	master, err := sarama.NewConsumer(brokers, config)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer func() {
-		if err := master.Close(); err != nil {
-			log.Panic(err)
-		}
-	}()
-	consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetOldest)
-	if err != nil {
-		log.Panic(err)
-	}
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-	doneCh := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case err := <-consumer.Errors():
-				log.Println(err)
-			case msg := <-consumer.Messages():
-				messageCountStart++
-				log.Println("Received messages", string(msg.Key), string(msg.Value))
-
-			case <-signals:
-				log.Println("Interrupt is detected")
-				doneCh <- struct{}{}
-			}
-		}
-	}()
-	<-doneCh
-	log.Println("Processed", messageCountStart, "messages")
-}
 
 type ExampleConsumerGroupHandler struct{}
 
